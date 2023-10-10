1c494bcb77a00d47cc071224eca677d8e9531470
luluazinha <128510873+luluazinha@users.noreply.github.com>
2023-10-10 15:38:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play('');
 
-document.querySelector('#som_tecla_pom').play('');
+}
+const listaDeTeclas = document.querySelector('.tecla');
 
+for (contador = 0; contador <listaDeTeclas.lenght; contador++){
+
+const intrumento = listaDeTeclas[contador].classList[1];
+
+const tecla = listaDeteclas[contador];
+
+const idAudio = '#som_${instrumento}'
+
+tecla.onclick = function(){
+    tocaSom(idAudio);
+};
+
+}
+
+
+
+
+
+
+
+
+}
 
